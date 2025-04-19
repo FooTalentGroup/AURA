91cc1c4d1d10c25cac633cc4d1c2ca49e37f2fca
lucianocopa45 <45108640@ifts24.edu.ar>
2025-04-18 22:00:34 -0300

    Agregue el modelo y los datos de las historias

--- src/main/java/com/clinica/aura/entities/medical_records/model/MedicalRecordsModel.java
+++ src/main/java/com/clinica/aura/entities/medical_records/model/MedicalRecordsModel.java
@@ -19,7 +19,7 @@ import java.time.LocalDate;
 @Builder
 @Table(name = "medical_records")
 public class MedicalRecordsModel {
-    //Id historia
+    //historia clinicas
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
