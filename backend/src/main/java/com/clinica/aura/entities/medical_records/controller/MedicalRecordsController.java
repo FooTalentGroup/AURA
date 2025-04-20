c3a53429b19718223e4aa7f6d4658c07c2ee8d88
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2025-04-20 10:56:00 -0500

    merge professionals

--- backend/src/main/java/com/clinica/aura/entities/medical_records/controller/MedicalRecordsController.java
+++ backend/src/main/java/com/clinica/aura/entities/medical_records/controller/MedicalRecordsController.java
@@ -17,29 +17,29 @@ import java.util.List;
 public class MedicalRecordsController {
     private final MedicalRecordsService medicalRecordsService;
 
-    @PostMapping
-    public ResponseEntity<MedicalRecordsModel> create(@Valid @RequestBody MedicalRecordsRequestDto dto) {
-        return ResponseEntity.ok(medicalRecordsService.create(dto));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<MedicalRecordsModel>> getAll() {
-        return ResponseEntity.ok(medicalRecordsService.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<MedicalRecordsModel> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(medicalRecordsService.findById(id));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<MedicalRecordsModel> update(@PathVariable Long id, @Valid @RequestBody MedicalRecordsRequestDto dto) {
-        return ResponseEntity.ok(medicalRecordsService.update(id, dto));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        medicalRecordsService.delete(id);
-        return ResponseEntity.noContent().build();
-    }
+//    @PostMapping
+//    public ResponseEntity<MedicalRecordsModel> create(@Valid @RequestBody MedicalRecordsRequestDto dto) {
+//        return ResponseEntity.ok(medicalRecordsService.create(dto));
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<MedicalRecordsModel>> getAll() {
+//        return ResponseEntity.ok(medicalRecordsService.findAll());
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<MedicalRecordsModel> getById(@PathVariable Long id) {
+//        return ResponseEntity.ok(medicalRecordsService.findById(id));
+//    }
+//
+//    @PutMapping("/{id}")
+//    public ResponseEntity<MedicalRecordsModel> update(@PathVariable Long id, @Valid @RequestBody MedicalRecordsRequestDto dto) {
+//        return ResponseEntity.ok(medicalRecordsService.update(id, dto));
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> delete(@PathVariable Long id) {
+//        medicalRecordsService.delete(id);
+//        return ResponseEntity.noContent().build();
+//    }
 }
