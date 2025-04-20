9516e231f345318fa47d776315c69c173d163e58
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2025-04-20 09:53:00 -0500

    merge patient

--- backend/src/main/java/com/clinica/aura/entities/patient/controller/PatientController.java
+++ backend/src/main/java/com/clinica/aura/entities/patient/controller/PatientController.java
@@ -6,8 +6,11 @@ import com.clinica.aura.entities.patient.dtoRequest.PatientRequestDto;
 import com.clinica.aura.entities.patient.dtoRequest.PatientResponseDto;
 import com.clinica.aura.entities.patient.service.PatientService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +22,16 @@ import java.util.List;
 public class PatientController {
 
     private final PatientService patientService;
+
+    @Operation(summary = "Registrar nuevo paciente", description = """
+            Registra un nuevo paciente
+            """)
+    @PostMapping(value = "/register")
+    @Tag(name = "Patient")
+    public ResponseEntity<PatientResponseDto> registerPatient(@RequestBody @Valid PatientRequestDto authCreateUserDto) {
+        PatientResponseDto response = patientService.createUser(authCreateUserDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 
     @GetMapping
     @Operation(summary = "Obtener todos los pacientes", description = "Devuelve una lista de todos los pacientes registrados en el sistema.")
