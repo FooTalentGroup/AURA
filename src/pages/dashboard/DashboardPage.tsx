5faa40df493228f1ab1353d7ea08b9f5b4a46f0a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-04-29 10:40:59 -0300

    feat: agregando DiagnosisForm a DashboardPage

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,6 +1,37 @@
 import { useContextAuth } from "../../features/auth/hooks/useContextAuth";
 import DashboardLayout from "../../layouts/DashboardLayout";
-import { FiEdit2, FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import {
+  FiEdit2,
+  FiChevronLeft,
+  FiChevronRight,
+  FiMoreHorizontal,
+} from "react-icons/fi";
+
+const DiagnosisForm = () => {
+  return (
+    <div className="w-full max-w-md mx-auto bg-white p-4">
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-semibold text-gray-800">Sesiones</h2>
+        <button className="text-gray-700 hover:text-black focus:outline-none cursor-pointer">
+          <FiMoreHorizontal size={20} />
+        </button>
+      </div>
+
+      <div className="mb-4">
+        <label className="block text-gray-800 font-medium mb-1">Día</label>
+      </div>
+
+      <div className="mb-2">
+        <label className="block text-gray-500 text-sm mb-1">Descripción</label>
+        <input
+          type="text"
+          placeholder="Ingrese una descripción"
+          className="w-full border border-gray-300 rounded p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+    </div>
+  );
+};
 
 const DashboardPage = () => {
   const { state } = useContextAuth();
@@ -22,7 +53,6 @@ const DashboardPage = () => {
               <p>00/00</p>
             </footer>
           </div>
-          <div></div>
         </div>
 
         <article className="col-span-2">
@@ -67,7 +97,7 @@ const DashboardPage = () => {
 
               <form className="grid grid-cols-2 gap-4">
                 <div className="col-span-1">
-                  <label className="block text-sm font-normal text-gray-500 mb-1">
+                  <label className="block text-sm font-semibold text-gray-500 mb-2">
                     Fecha de nacimiento
                   </label>
                   <input
@@ -80,7 +110,7 @@ const DashboardPage = () => {
                 </div>
 
                 <div className="col-span-1">
-                  <label className="block text-sm font-normal text-gray-500 mb-1">
+                  <label className="block text-sm font-semibold text-gray-500 mb-2">
                     Edad
                   </label>
                   <input
@@ -93,7 +123,7 @@ const DashboardPage = () => {
                 </div>
 
                 <div className="col-span-2">
-                  <label className="block text-sm font-normal text-gray-500 mb-1">
+                  <label className="block text-sm font-semibold text-gray-500 mb-2">
                     DNI
                   </label>
                   <input
@@ -109,23 +139,26 @@ const DashboardPage = () => {
 
             <div className="bg-white p-4">
               <form className="">
-                <div className="col-span-1">
-                  <label className="block text-sm font-normal text-gray-500 mb-1">
-                    Diagnóstico
-                  </label>
-                  <input
-                    type="text"
-                    name="birthDate"
-                    placeholder="Escriba el diagnóstico"
-                    value={
-                      "Paciente masculino de 37 años con antecedentes de hipertensión arterial controlada. Presenta dolor lumbar crónico irradiado a pierna izquierda, asociado a hernia de disco L4-L5 confirmada por resonancia. Refiere limitación funcional moderada. En tratamiento con kinesiología y AINEs."
-                    }
-                    className="w-full py-2 px-3 bg-gray-100 rounded text-gray-700"
-                  />
-                </div>
+                <header className="flex justify-between items-center mb-4">
+                  <h2 className="">Diagnóstico</h2>
+                  <button>
+                    <FiEdit2 size={20} />
+                  </button>
+                </header>
+                <input
+                  type="text"
+                  name="birthDate"
+                  placeholder="Escriba el diagnóstico"
+                  value={
+                    "Paciente masculino de 37 años con antecedentes de hipertensión arterial controlada. Presenta dolor lumbar crónico irradiado a pierna izquierda, asociado a hernia de disco L4-L5 confirmada por resonancia. Refiere limitación funcional moderada. En tratamiento con kinesiología y AINEs."
+                  }
+                  className="w-full py-2 px-3 bg-gray-100 rounded text-gray-700"
+                />
               </form>
             </div>
           </article>
+          <DiagnosisForm />
+          <DiagnosisForm />
         </article>
       </div>
     </DashboardLayout>
