648507fd501833b60cb526477b84e2abdfe461b9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-01 13:23:40 -0300

    feat:segundo Home en Dashboard armado

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -112,7 +112,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       </header>
 
       <div className="flex flex-grow">
-        <aside className="flex-grow flex justify-center items-center">
+        <aside className="flex-grow flex justify-center items-center max-w-24">
           <nav className="px-1">
             <ul className="text-gray-800 flex flex-col gap-4 h-full text-center [&>li>a]:flex-col [&>li>a]:hover:text-blue-700 [&>li>a]:hover:bg-sky-200/60">
               <li className="">
--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -6,7 +6,7 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 const PatientItemList = () => {
   return (
     <div className="text-neutral-600">
-      <ul className="flex justify-between items-center">
+      <ul className="flex justify-between items-center gap-8">
         <li className="flex gap-2">
           <span className="flex items-center font-semibold text-sm bg-gray-200/60 px-3 py-[1px] rounded-xl">
             12:00
@@ -26,11 +26,11 @@ const PatientItemList = () => {
         </li>
         <li className="">44.617.293</li>
         <li className="">OSPLAD</li>
-        <li className="">
+        {/* <li className="">
           <button className="text-blue-600 py-2 px-5 border border-black rounded-4xl hover:bg-gray-200 cursor-pointer">
             Ver paciente
           </button>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
@@ -39,16 +39,79 @@ const PatientItemList = () => {
 const MedicalHistory = () => {
   return (
     <article>
-      <h2 className="flex items-center gap-2 text-xl font-semibold mb-4">
-        {" "}
-        <ClipBoardIcon /> Historial clínico
-      </h2>
-      <ul className="[&>li]:mb-2 text-sm">
+      <header className="flex justify-between items-center mb-6">
+        <h2 className="flex items-center gap-2 text-xl font-semibold">
+          {" "}
+          <ClipBoardIcon /> Historial clínico
+        </h2>
+        <button className="text-blue-600 py-2 px-5 border border-black rounded-4xl hover:bg-gray-200 cursor-pointer">
+          Ver historial
+        </button>
+      </header>
+      {/* <ul className="[&>li]:mb-2 text-sm">
         <li>05/04/2025 — Audiometría infantil — Normal</li>
         <li>20/03/2025 — Evaluación neurológica — Seguimiento recomendado</li>
         <li>12/02/2025 — Test de lenguaje — Déficit leve</li>
-      </ul>
+      </ul> */}
+      <MedicalHistoryTable />
     </article>
+  );
+};
+
+const MedicalHistoryTable = () => {
+  return (
+    <div className="bg-white rounded-lg overflow-hidden">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50 ">
+          <tr>
+            <th className="px-2 py-3 text-left text-xs font-medium uppercase tracking-wider">
+              Fecha
+            </th>
+            <th className="px-2 py-3 text-left text-xs font-medium uppercase tracking-wider">
+              Título
+            </th>
+            <th className="px-2 py-3 text-left text-xs font-medium uppercase tracking-wider">
+              Observación
+            </th>
+            <th className="px-2 py-3 text-left text-xs font-medium uppercase tracking-wider">
+              Especialidad
+            </th>
+            <th className="px-2 py-3 text-left text-xs font-medium uppercase tracking-wider">
+              Profesional
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          <tr>
+            <td className="px-2 py-4 whitespace-nowrap">05 Abr 2025</td>
+            <td className="px-2 py-4 whitespace-nowrap">
+              Audiometría infantil
+            </td>
+            <td className="px-2 py-4 whitespace-nowrap">Normal</td>
+            <td className="px-2 py-4 whitespace-nowrap">Fonoaudiología</td>
+            <td className="px-2 py-4 whitespace-nowrap">Dr. José Hernández</td>
+          </tr>
+          <tr>
+            <td className="px-2 py-4 whitespace-nowrap">20 Mar 2025</td>
+            <td className="px-2 py-4 whitespace-nowrap">
+              Evaluación neurológica
+            </td>
+            <td className="px-2 py-4 whitespace-nowrap">
+              Seguimiento recomendado
+            </td>
+            <td className="px-2 py-4 whitespace-nowrap">Psicología</td>
+            <td className="px-2 py-4 whitespace-nowrap">Dr. Carina Ledesma</td>
+          </tr>
+          <tr>
+            <td className="px-2 py-4 whitespace-nowrap">12 Feb 2025</td>
+            <td className="px-2 py-4 whitespace-nowrap">Test de lenguaje</td>
+            <td className="px-2 py-4 whitespace-nowrap">Déficit leve</td>
+            <td className="px-2 py-4 whitespace-nowrap">Psicopedagogía</td>
+            <td className="px-2 py-4 whitespace-nowrap">Dr. Laura Torres</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
@@ -95,41 +158,41 @@ const ClipBoardIcon = () => {
   );
 };
 
-const CalendarIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6 text-blue-600"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-      />
-    </svg>
-  );
-};
+// const CalendarIcon = () => {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       fill="none"
+//       viewBox="0 0 24 24"
+//       strokeWidth={1.5}
+//       stroke="currentColor"
+//       className="size-6 text-blue-600"
+//     >
+//       <path
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+//       />
+//     </svg>
+//   );
+// };
 
-const CheckIcon = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      className="size-6 text-blue-600"
-    >
-      <path
-        fillRule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-};
+// const CheckIcon = () => {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       viewBox="0 0 24 24"
+//       fill="currentColor"
+//       className="size-6 text-blue-600"
+//     >
+//       <path
+//         fillRule="evenodd"
+//         d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+//         clipRule="evenodd"
+//       />
+//     </svg>
+//   );
+// };
 
 const DashboardPage = () => {
   // const { state } = useContextAuth();
@@ -140,7 +203,7 @@ const DashboardPage = () => {
         Bienvenido {state.user?.email}
       </h1> */}
       <div className="grid grid-cols-7 gap-4 rounded-lg">
-        <div className="col-span-2 mb-6">
+        {/* <div className="col-span-2 mb-6">
           <div className="bg-white p-4 rounded-2xl">
             <h2 className="text-xl mb-6">Turnos de hoy</h2>
 
@@ -160,10 +223,13 @@ const DashboardPage = () => {
               </article>
             </div>
           </div>
-        </div>
+        </div> */}
 
-        <article className="col-span-5 flex flex-col gap-4">
-          <div className="flex justify-between items-center bg-white py-6 px-4 rounded-2xl">
+        <article className="col-span-7 flex flex-col gap-4 bg-white rounded-2xl">
+          <h2 className="text-2xl pt-6 pb-6 px-4 border-b-2 border-gray-300/90">
+            Turnos de hoy, <span className="text-blue-600">Sábado 5</span>
+          </h2>
+          {/* <div className="flex justify-between items-center bg-white py-6 px-4 rounded-2xl">
             <h2 className="text-xl">Lucas Fernández</h2>
 
             <div className="flex gap-4">
@@ -175,18 +241,37 @@ const DashboardPage = () => {
                 <FaChevronRight />
               </button>
             </div>
-          </div>
+          </div> */}
           <div className="flex flex-col bg-white rounded-2xl p-6">
-            <div className="border-b border-gray-400 pb-6">
+            <div className="flex justify-between items-center border- border-gray-400 pb-6">
               <PatientItemList />
+              <div className="flex gap-4">
+                <p className="flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="size-5"
+                  >
+                    <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
+                  </svg>
+                  <span className="font-semibold text-gray-500">1/12</span>
+                </p>
+                <button className="text-gray-500 hover:text-gray-900 cursor-pointer">
+                  <FaChevronLeft />
+                </button>
+                <button className="text-gray-500 hover:text-gray-900 cursor-pointer">
+                  <FaChevronRight />
+                </button>
+              </div>
             </div>
-            <div className="border-b border-gray-400 py-6">
+            <div className="border- border-gray-400 py-6">
               <MedicalHistory />
             </div>
-            <div className="border-b border-gray-400 py-6">
+            <div className="border- border-gray-400 py-6">
               <PatientNotes />
             </div>
-            <footer className="flex justify-end gap-4 pt-6">
+            {/* <footer className="flex justify-end gap-4 pt-6">
               <select
                 className="border border-gray-400 px-2 rounded-lg cursor-pointer"
                 name=""
@@ -199,7 +284,7 @@ const DashboardPage = () => {
               <button className="text-white py-2 px-5 bg-blue-500 rounded-4xl hover:bg-blue-700 cursor-pointer">
                 Llamar
               </button>
-            </footer>
+            </footer> */}
           </div>
         </article>
       </div>
