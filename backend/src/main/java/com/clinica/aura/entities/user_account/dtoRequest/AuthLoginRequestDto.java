6696317add3459aa68b62407693c38d2396a61ce
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2025-05-01 18:28:29 -0500

    refactor: valid email login

--- backend/src/main/java/com/clinica/aura/entities/user_account/dtoRequest/AuthLoginRequestDto.java
+++ backend/src/main/java/com/clinica/aura/entities/user_account/dtoRequest/AuthLoginRequestDto.java
@@ -1,6 +1,7 @@
 package com.clinica.aura.entities.user_account.dtoRequest;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import lombok.Setter;
 public class AuthLoginRequestDto {
     @NotBlank(message = "El email es obligatorio")
     @Schema(description = "Correo del usuario", requiredMode = Schema.RequiredMode.REQUIRED, example = "admin@example.com")
+    @Email(message = "El correo electrónico no es válido")
     private String email;
     @NotBlank(message = "La contraseña es obligatoria")
     @Schema(description = "Contraseña del usuario",requiredMode = Schema.RequiredMode.REQUIRED, example = "admin123")
