f96313b9ae760c993a1b2021dba5df1277afffd5
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2025-05-04 17:12:57 -0500

    fix: CORS configuration

--- backend/src/main/java/com/clinica/aura/config/SecurityConfig.java
+++ backend/src/main/java/com/clinica/aura/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -36,8 +37,21 @@ public class SecurityConfig {
     private final JwtUtils jwtUtils;
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)  // Deshabilitar CSRF completamente
                 .authorizeHttpRequests(authorize -> authorize
                         //Rutas públicas
@@ -89,16 +103,4 @@ public class SecurityConfig {
     }
 
 
-    //Habilitar CORS para permitir todos los endpoints
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(Boolean.TRUE);
-        config.addAllowedOriginPattern("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
 }
