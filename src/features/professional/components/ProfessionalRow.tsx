49583cf25638bc55e6b099f8cb4817e898a93016
adrian161103 <adriankeomaa@gmail.com>
2025-05-17 09:56:05 -0300

    alineacion en personal

--- src/features/professional/components/ProfessionalRow.tsx
+++ src/features/professional/components/ProfessionalRow.tsx
@@ -36,7 +36,7 @@ export const ProfessionalRow: React.FC<Props> = ({ professional, onView, onViewS
 
   return (
     <>
-      <div className="grid grid-cols-[2fr_1fr_1fr_1fr_1fr_1fr_1fr] items-center bg-white rounded-xl shadow-sm px-4 py-3 mb-3">
+      <div className=" grid grid-cols-[2fr_1fr_1fr_1fr_1fr_auto] items-center bg-white rounded-xl shadow-sm px-4 py-3 mb-3">
         {/* Avatar e info básica */}
         <div className="flex items-center space-x-4">
           <div className="w-10 h-10 rounded-full bg-green-100 text-green-700 flex items-center justify-center font-bold">
